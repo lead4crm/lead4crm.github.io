e86b4d9dc9c373f653f5f57dc374dfa3cee8b43f
Arsen Bespalov <me@arsen.pw>
2015-06-20 09:02:26 +0800

    update blog

--- _plugins/i18n_filter.rb
+++ _plugins/i18n_filter.rb
@@ -1,6 +1,10 @@
+require 'date'
 require 'i18n'
 
-LOCALE = :ru # set your locale
+# LOCALE = 'ru' # set your locale
+path = File.expand_path("../../_locales/ru.yml", __FILE__)
+I18n.load_path = Dir[path]
+I18n.locale = :ru
 
 # Create folder "_locales" and put some locale file from https://github.com/svenfuchs/rails-i18n/tree/master/rails/locale
 module Jekyll
@@ -9,9 +13,18 @@ module Jekyll
     #   {{ post.date | localize: "%d.%m.%Y" }}
     #   {{ post.date | localize: ":short" }}
     def localize(input, format=nil)
-      load_translations
+      # load_translations
       format = (format =~ /^:(\w+)/) ? $1.to_sym : format
+
+      if input.is_a?(String)
+        input = DateTime.parse(input)
+      end
+
       I18n.l input, :format => format
+    end
+
+    def translate(key)
+      I18n.t key
     end
 
     def load_translations
